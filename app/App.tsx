bc3a49f8e2b8a3e1131d127176d29be8585140b8
Cuong_Do <mamxanh5555@gmail.com>
2025-03-24 10:50:13 +0700

    Location Pick Page

--- app/App.tsx
+++ app/App.tsx
@@ -4,7 +4,8 @@ import AuthMethod from "../screens/authentication/AuthMethod";
 import OTPVerify from "../screens/authentication/OTPVerify";
 import UserInfo from "../screens/authentication/UserInfo";
 import Order from "../screens/order/Order";
+import LocationPick from "../screens/order/LocationPick";
 
 export default function App() {
-  return <Order />;
+  return <LocationPick />;
 }

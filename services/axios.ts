60891a87c4321c073cb81e0bf1a0adb697f94743
nchuy099 <nchihuy099@gmail.com>
2025-04-11 16:53:07 +0700

    fix connect to phone

--- services/axios.ts
+++ services/axios.ts
@@ -1,17 +1,37 @@
 import axios from 'axios';
 import authService from './auth.service';
+import * as Keychain from 'react-native-keychain';
 
-const instance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+// Helper function to get token from keychain
+const getToken = async (): Promise<string | null> => {
+    try {
+        const credentials = await Keychain.getGenericPassword({ service: 'auth' });
+        if (credentials) {
+            return credentials.password;
+        }
+        return null;
+    } catch (error) {
+        console.error('Error retrieving token from keychain:', error);
+        return null;
+    }
+};
+
+const tokenAxios = axios.create({
+    baseURL: 'http://192.168.47.110:3000/api',
     headers: {
         'Content-Type': 'application/json',
     },
 });
 
 // Add a request interceptor
-instance.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('accessToken');
+tokenAxios.interceptors.request.use(
+    async (config) => {
+        if ((config as any).skipAuth) {
+            return config; // Bỏ qua thêm token
+        }
+
+        const token = await getToken();
+        console.log(1);
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
@@ -22,5 +42,11 @@ instance.interceptors.request.use(
     }
 );
 
+const plainAxios = axios.create({
+    baseURL: 'http://192.168.47.110:3000/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
 
-export default instance;
+export { tokenAxios, plainAxios };
